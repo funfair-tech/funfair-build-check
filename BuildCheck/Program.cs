1b3d02a79a471e88a8d6106cd1e7a7e454860c12
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-02-12 13:34:52 +0000

    Added basic project structure

--- BuildCheck/Program.cs
+++ BuildCheck/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace BuildCheck
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private const int SUCCESS = 0;
+        private const int ERROR = 1;
+
+        private static void Usage()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Usage:");
+            Console.WriteLine($"{typeof(Program).Namespace} -Solution D:\\Source\\Solution.sln [-WarningAsErrors true|false]");
+        }
+
+        public static int Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"{typeof(Program).Namespace} {ExecutableVersionInformation.ProgramVersion()}");
+
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .AddCommandLine(args, new Dictionary<string, string> {{@"-Solution", @"solution"}, {@"-WarningAsErrors", @"WarningAsErrors"}})
+                    .Build();
+
+                string solution = configuration.GetValue<string>(@"solution");
+                if (string.IsNullOrWhiteSpace(solution))
+                {
+                    Usage();
+                    return ERROR;
+                }
+
+                if (!File.Exists(solution))
+                {
+                    Console.WriteLine("Missing Solution file.");
+                    Usage();
+                    return ERROR;
+                }
+
+                bool warningsAsErrors = configuration.GetValue<bool>(@"WarningAsErrors");
+                Console.WriteLine($"{warningsAsErrors}");
+
+                return SUCCESS;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"ERROR: {exception.Message}");
+                return ERROR;
+            }
         }
     }
 }
