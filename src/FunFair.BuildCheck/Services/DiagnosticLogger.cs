8a935c467105abe2b7c2500d819704458a85c1a5
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-11-08 10:50:32 +0000

    Merged into pattern

--- src/FunFair.BuildCheck/Services/DiagnosticLogger.cs
+++ src/FunFair.BuildCheck/Services/DiagnosticLogger.cs
@@ -92,7 +92,7 @@ namespace FunFair.BuildCheck.Services
 
         private static bool IsError(LogLevel logLevel)
         {
-            return logLevel == LogLevel.Critical || logLevel == LogLevel.Error;
+            return logLevel is LogLevel.Critical or LogLevel.Error;
         }
 
         private bool IsWarningAsError(LogLevel logLevel)
