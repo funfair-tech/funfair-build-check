5c125b86a378e7d5d408037ff49aa2ea40ddccd0
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-11-17 22:38:06 +0000

    Used namespace scope

--- src/FunFair.BuildCheck.ProjectChecks/Settings/TargetFrameworkIsSetCorrectlyPolicy.cs
+++ src/FunFair.BuildCheck.ProjectChecks/Settings/TargetFrameworkIsSetCorrectlyPolicy.cs
@@ -5,60 +5,61 @@ using FunFair.BuildCheck.Interfaces;
 using FunFair.BuildCheck.ProjectChecks.Helpers;
 using Microsoft.Extensions.Logging;
 
-namespace FunFair.BuildCheck.ProjectChecks.Settings;
-
-/// <summary>
-///     Checks to see if the TargetFramework/TargetFrameworks is set correctly.
-/// </summary>
-[SuppressMessage(category: "ReSharper", checkId: "ClassNeverInstantiated.Global", Justification = "Created by DI")]
-public sealed class TargetFrameworkIsSetCorrectlyPolicy : IProjectCheck
+namespace FunFair.BuildCheck.ProjectChecks.Settings
 {
-    private readonly IRepositorySettings _repositorySettings;
-    private readonly ILogger<TargetFrameworkIsSetCorrectlyPolicy> _logger;
-    private readonly string? _expected;
-
     /// <summary>
-    ///     Constructor.
+    ///     Checks to see if the TargetFramework/TargetFrameworks is set correctly.
     /// </summary>
-    /// <param name="repositorySettings">Repository settings</param>
-    /// <param name="logger">Logging.</param>
-    public TargetFrameworkIsSetCorrectlyPolicy(IRepositorySettings repositorySettings, ILogger<TargetFrameworkIsSetCorrectlyPolicy> logger)
+    [SuppressMessage(category: "ReSharper", checkId: "ClassNeverInstantiated.Global", Justification = "Created by DI")]
+    public sealed class TargetFrameworkIsSetCorrectlyPolicy : IProjectCheck
     {
-        this._repositorySettings = repositorySettings;
-        this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        this._expected = Environment.GetEnvironmentVariable("DOTNET_CORE_APP_TARGET_FRAMEWORK");
-    }
+        private readonly IRepositorySettings _repositorySettings;
+        private readonly ILogger<TargetFrameworkIsSetCorrectlyPolicy> _logger;
+        private readonly string? _expected;
 
-    /// <inheritdoc />
-    public void Check(string projectName, string projectFolder, XmlDocument project)
-    {
-        if (this._repositorySettings.IsCodeAnalysisSolution && !project.IsTestProject(projectName: projectName, logger: this._logger))
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        /// <param name="repositorySettings">Repository settings</param>
+        /// <param name="logger">Logging.</param>
+        public TargetFrameworkIsSetCorrectlyPolicy(IRepositorySettings repositorySettings, ILogger<TargetFrameworkIsSetCorrectlyPolicy> logger)
         {
-            // Code analysis project has specific requirements
-            ProjectValueHelpers.CheckValue(projectName: projectName, project: project, nodePresence: @"TargetFramework", requiredValue: "netstandard2.0", logger: this._logger);
-
-            return;
+            this._repositorySettings = repositorySettings;
+            this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this._expected = Environment.GetEnvironmentVariable("DOTNET_CORE_APP_TARGET_FRAMEWORK");
         }
 
-        if (string.IsNullOrEmpty(this._expected))
+        /// <inheritdoc />
+        public void Check(string projectName, string projectFolder, XmlDocument project)
         {
-            // no frameworks defined - allow any
-            return;
-        }
+            if (this._repositorySettings.IsCodeAnalysisSolution && !project.IsTestProject(projectName: projectName, logger: this._logger))
+            {
+                // Code analysis project has specific requirements
+                ProjectValueHelpers.CheckValue(projectName: projectName, project: project, nodePresence: @"TargetFramework", requiredValue: "netstandard2.0", logger: this._logger);
 
-        string[] frameworks = this._expected.Split(";");
+                return;
+            }
 
-        switch (frameworks.Length)
-        {
-            case 0: return;
-            case 1:
-                ProjectValueHelpers.CheckValue(projectName: projectName, project: project, nodePresence: @"TargetFramework", frameworks[0], logger: this._logger);
+            if (string.IsNullOrEmpty(this._expected))
+            {
+                // no frameworks defined - allow any
+                return;
+            }
 
-                break;
-            default:
-                ProjectValueHelpers.CheckValue(projectName: projectName, project: project, nodePresence: @"TargetFrameworks", requiredValue: this._expected, logger: this._logger);
+            string[] frameworks = this._expected.Split(";");
 
-                break;
+            switch (frameworks.Length)
+            {
+                case 0: return;
+                case 1:
+                    ProjectValueHelpers.CheckValue(projectName: projectName, project: project, nodePresence: @"TargetFramework", frameworks[0], logger: this._logger);
+
+                    break;
+                default:
+                    ProjectValueHelpers.CheckValue(projectName: projectName, project: project, nodePresence: @"TargetFrameworks", requiredValue: this._expected, logger: this._logger);
+
+                    break;
+            }
         }
     }
 }
