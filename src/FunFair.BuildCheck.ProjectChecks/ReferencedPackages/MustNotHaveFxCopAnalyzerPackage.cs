5647215ba4a1e6c178844462452de3a8dec54235
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-12-19 20:36:20 +0000

    Checking for Microsoft.CodeAnalysis.VersionCheckAnalyzer too

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustNotHaveFxCopAnalyzerPackage.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustNotHaveFxCopAnalyzerPackage.cs
@@ -14,7 +14,7 @@ public sealed class MustNotHaveFxCopAnalyzerPackage : MustNotReferencePackages
     /// </summary>
     /// <param name="logger">Logging.</param>
     public MustNotHaveFxCopAnalyzerPackage(ILogger<MustNotHaveFxCopAnalyzerPackage> logger)
-        : base(new[] { "Microsoft.CodeAnalysis.FxCopAnalyzers" }, reason: ".net core 5 and later include this by default", logger: logger)
+        : base(new[] { "Microsoft.CodeAnalysis.FxCopAnalyzers", "Microsoft.CodeAnalysis.VersionCheckAnalyzer" }, reason: ".net core 5 and later include this by default", logger: logger)
     {
     }
 }
