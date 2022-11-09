0a940140aacfeef95f34477108850ddd3ac6ca51
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-11-09 19:46:28 +0000

    Stupidity!

--- src/FunFair.BuildCheck.Helpers/EnumExtensions.cs
+++ src/FunFair.BuildCheck.Helpers/EnumExtensions.cs
@@ -2,10 +2,10 @@ using System.Diagnostics.CodeAnalysis;
 using Credfeto.Enumeration.Source.Generation.Attributes;
 using Microsoft.Extensions.Logging;
 
-namespace FunFair.BuildCheck.Services;
+namespace FunFair.BuildCheck.Helpers;
 
 [EnumText(typeof(LogLevel))]
 [SuppressMessage(category: "ReSharper", checkId: "PartialTypeWithSinglePart", Justification = "Needed for generated code")]
-internal static partial class EnumExtensions
+public static partial class EnumExtensions
 {
 }
