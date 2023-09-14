e7a8b75f683306dc2633414633a7e7a6968bf139
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-09-14 18:23:11 +0100

    Fixed code analysis issues

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustNotHaveFxCopAnalyzerPackage.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustNotHaveFxCopAnalyzerPackage.cs
@@ -4,14 +4,14 @@ namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages;
 
 public sealed class MustNotHaveFxCopAnalyzerPackage : MustNotReferencePackages
 {
+    private static readonly string[] PackageIds =
+    {
+        "Microsoft.CodeAnalysis.FxCopAnalyzers",
+        "Microsoft.CodeAnalysis.VersionCheckAnalyzer"
+    };
+
     public MustNotHaveFxCopAnalyzerPackage(ILogger<MustNotHaveFxCopAnalyzerPackage> logger)
-        : base(new[]
-               {
-                   "Microsoft.CodeAnalysis.FxCopAnalyzers",
-                   "Microsoft.CodeAnalysis.VersionCheckAnalyzer"
-               },
-               reason: ".net core 5 and later include this by default",
-               logger: logger)
+        : base(PackageIds, reason: ".net core 5 and later include this by default", logger: logger)
     {
     }
 }
