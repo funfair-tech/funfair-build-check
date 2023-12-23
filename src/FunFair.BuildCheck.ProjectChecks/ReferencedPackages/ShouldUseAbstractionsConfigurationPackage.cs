09a682405a372218c71e60e28840bfc316364e19
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-12-23 21:05:12 +0000

    Don't check usage of DI Abstractions when project is Unit tests base

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/ShouldUseAbstractionsConfigurationPackage.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/ShouldUseAbstractionsConfigurationPackage.cs
@@ -1,21 +1,11 @@
-using System.Xml;
-using FunFair.BuildCheck.Interfaces;
 using Microsoft.Extensions.Logging;
 
 namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages;
 
 public sealed class ShouldUseAbstractionsConfigurationPackage : ShouldUseAlternatePackage
 {
-    private readonly IRepositorySettings _repositorySettings;
-
-    public ShouldUseAbstractionsConfigurationPackage(IRepositorySettings repositorySettings, ILogger<ShouldUseAbstractionsConfigurationPackage> logger)
+    public ShouldUseAbstractionsConfigurationPackage(ILogger<ShouldUseAbstractionsConfigurationPackage> logger)
         : base(matchPackageId: "Microsoft.Extensions.Configuration", usePackageId: "Microsoft.Extensions.Configuration.Abstractions", logger: logger)
     {
-        this._repositorySettings = repositorySettings;
-    }
-
-    protected override bool CanCheck(string projectName, string projectFolder, XmlDocument project)
-    {
-        return !this._repositorySettings.IsUnitTestBase;
     }
 }
--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/ShouldUseAbstractionsDependencyInjectionPackage.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/ShouldUseAbstractionsDependencyInjectionPackage.cs
@@ -1,11 +1,21 @@
+using System.Xml;
+using FunFair.BuildCheck.Interfaces;
 using Microsoft.Extensions.Logging;
 
 namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages;
 
 public sealed class ShouldUseAbstractionsDependencyInjectionPackage : ShouldUseAlternatePackage
 {
-    public ShouldUseAbstractionsDependencyInjectionPackage(ILogger<ShouldUseAbstractionsDependencyInjectionPackage> logger)
+    private readonly IRepositorySettings _repositorySettings;
+
+    public ShouldUseAbstractionsDependencyInjectionPackage(IRepositorySettings repositorySettings, ILogger<ShouldUseAbstractionsDependencyInjectionPackage> logger)
         : base(matchPackageId: "Microsoft.Extensions.DependencyInjection", usePackageId: "Microsoft.Extensions.DependencyInjection.Abstractions", logger: logger)
     {
+        this._repositorySettings = repositorySettings;
+    }
+
+    protected override bool CanCheck(string projectName, string projectFolder, XmlDocument project)
+    {
+        return !this._repositorySettings.IsUnitTestBase;
     }
 }
