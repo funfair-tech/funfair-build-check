5147306edd54ce315e5aa1ac3664235b2f70693e
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-01-01 16:02:24 +0000

    Source generated logging

--- src/FunFair.BuildCheck.ProjectChecks/References/DoesNotReferenceByDll.cs
+++ src/FunFair.BuildCheck.ProjectChecks/References/DoesNotReferenceByDll.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using FunFair.BuildCheck.Interfaces;
+using FunFair.BuildCheck.ProjectChecks.References.LoggingExtensions;
 using Microsoft.Extensions.Logging;
 
 namespace FunFair.BuildCheck.ProjectChecks.References;
@@ -28,7 +29,7 @@ public sealed class DoesNotReferenceByDll : IProjectCheck
         foreach (XmlElement reference in references.OfType<XmlElement>())
         {
             string assembly = reference.GetAttribute(name: "Include");
-            this._logger.LogError($"{projectName}: References {assembly} directly not using NuGet or a project reference.");
+            this._logger.ReferencesAssmbyDirectlyRatherThanThroughReference(projectName: projectName, packageId: assembly);
         }
 
         return ValueTask.CompletedTask;
---
+++ src/FunFair.BuildCheck.ProjectChecks/References/LoggingExtensions/DoesNotReferenceByDllLoggingExtensions.cs
@@ -0,0 +1,9 @@
+using Microsoft.Extensions.Logging;
+
+namespace FunFair.BuildCheck.ProjectChecks.References.LoggingExtensions;
+
+internal static partial class DoesNotReferenceByDllLoggingExtensions
+{
+    [LoggerMessage(EventId = 1, Level = LogLevel.Error, Message = "{projectName}: References {assembly} directly not using NuGet or a project reference.")]
+    public static partial void ReferencesAssmbyDirectlyRatherThanThroughReference(this ILogger<DoesNotReferenceByDll> logger, string projectName, string packageId);
+}
