de47769efc7a562db02984cb5a2916f13aa2b27a
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-01-02 10:36:32 +0000

    Made message consistent

--- src/FunFair.BuildCheck.ProjectChecks/Settings/LoggingExtensions/DoesNotUseRootNamespaceLoggingExtensions.cs
+++ src/FunFair.BuildCheck.ProjectChecks/Settings/LoggingExtensions/DoesNotUseRootNamespaceLoggingExtensions.cs
@@ -4,6 +4,6 @@ namespace FunFair.BuildCheck.ProjectChecks.Settings.LoggingExtensions;
 
 internal static partial class DoesNotUseRootNamespaceLoggingExtensions
 {
-    [LoggerMessage(EventId = 1, Level = LogLevel.Error, Message = "{projectName} Uses the RootNamespace setting to rename the namespace, when it should not")]
+    [LoggerMessage(EventId = 1, Level = LogLevel.Error, Message = "{projectName}: Uses the RootNamespace setting to rename the namespace, when it should not")]
     public static partial void UsesRootNamespace(this ILogger<DoesNotUseRootNamespace> logger, string projectName);
 }
