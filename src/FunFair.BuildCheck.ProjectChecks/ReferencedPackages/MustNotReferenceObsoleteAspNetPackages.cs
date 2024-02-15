4217768ae6285c5bb3a0311877f428da3d2ce8d3
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-02-15 19:03:16 +0000

    [Cleanup] - Code cleanup on FunFair.BuildCheck.sln

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustNotReferenceObsoleteAspNetPackages.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustNotReferenceObsoleteAspNetPackages.cs
@@ -90,7 +90,9 @@ public sealed class MustNotReferenceObsoleteAspNetPackages : MustNotReferencePac
     ];
 
     public MustNotReferenceObsoleteAspNetPackages(ILogger<MustNotReferenceObsoleteAspNetPackages> logger)
-        : base(packageIds: PackageIds, reason: "Obsoleted as direct reference with .net core 3.1 use the implicit reference through <Project Sdk=\"Microsoft.NET.Sdk.Web\">", logger: logger)
+        : base(packageIds: PackageIds,
+               reason: "Obsoleted as direct reference with .net core 3.1 use the implicit reference through <Project Sdk=\"Microsoft.NET.Sdk.Web\">",
+               logger: logger)
     {
     }
 }
