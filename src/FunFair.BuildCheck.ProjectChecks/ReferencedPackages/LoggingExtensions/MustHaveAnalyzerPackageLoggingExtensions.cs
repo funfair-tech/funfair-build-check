d5a6720e03ebe7a09178141d7faa83eabb6b2a75
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-08-26 15:08:52 +0100

    Removed chedk

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/LoggingExtensions/MustHaveAnalyzerPackageLoggingExtensions.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/LoggingExtensions/MustHaveAnalyzerPackageLoggingExtensions.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.Logging;
-
 namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages.LoggingExtensions;
 
 internal static partial class MustHaveAnalyzerPackageLoggingExtensions
@@ -7,6 +5,6 @@ internal static partial class MustHaveAnalyzerPackageLoggingExtensions
     [LoggerMessage(EventId = 1, Level = LogLevel.Error, Message = "{projectName}: Does not reference {packageId} with a PrivateAssets=\"{privateAssets}\" attribute")]
     public static partial void DoesNotUsePrivateAssetsAttribute(this ILogger logger, string projectName, string packageId, string privateAssets);
 
-    [LoggerMessage(EventId = 2, Level = LogLevel.Error, Message = "{projectName}: Does not reference {packageId}  using NuGet")]
+    [LoggerMessage(EventId = 2, Level = LogLevel.Error, Message = "{projectName}: Does not reference {packageId} using NuGet")]
     public static partial void DoesNotUseNuGet(this ILogger logger, string projectName, string packageId);
 }
