5203a60666fd5467b39ff21b3a8049ce2f6fbd69
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-02-12 15:29:30 +0000

    Csharpier

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ Please ADD ALL Changes to the UNRELEASED SECTION and not a specific release
 
 ## [Unreleased]
 ### Added
+
+- Check for CSharpier.MSBuild
 ### Fixed
 ### Changed
 - Dependencies - Updated Nullable.Extended.Analyzer to 1.15.6495
--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustHaveSecurityCodeScanAnalyzerPackage.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustHaveSecurityCodeScanAnalyzerPackage.cs
@@ -5,5 +5,7 @@ namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages;
 public sealed class MustHaveSecurityCodeScanAnalyzerPackage : MustHaveAnalyzerPackage
 {
     public MustHaveSecurityCodeScanAnalyzerPackage(ILogger<MustHaveSecurityCodeScanAnalyzerPackage> logger)
-        : base(packageId: "SecurityCodeScan.VS2019", mustHave: true, logger: logger) { }
+        : base(packageId: "SecurityCodeScan.VS2019", mustHave: true, logger: logger)
+    {
+    }
 }
