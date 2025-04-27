efec75343ba5d82ead93953e1f959813ce8e1c2a
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 07:59:19 +0000

    Cleanup: XmlDocumentationFileProhibitedPolicyLoggingExtensions.cs

--- src/FunFair.BuildCheck.ProjectChecks/Settings/LoggingExtensions/XmlDocumentationFileProhibitedPolicyLoggingExtensions.cs
+++ src/FunFair.BuildCheck.ProjectChecks/Settings/LoggingExtensions/XmlDocumentationFileProhibitedPolicyLoggingExtensions.cs
@@ -4,11 +4,7 @@ namespace FunFair.BuildCheck.ProjectChecks.Settings.LoggingExtensions;
 
 internal static partial class XmlDocumentationFileProhibitedPolicyLoggingExtensions
 {
-    [LoggerMessage(
-        EventId = 1,
-        Level = LogLevel.Error,
-        Message = "{projectName}: Should not have XML Documentation"
-    )]
+    [LoggerMessage(EventId = 1, Level = LogLevel.Error, Message = "{projectName}: Should not have XML Documentation")]
     public static partial void ShouldNotHaveXmlDocumentation(
         this ILogger<XmlDocumentationFileProhibitedPolicy> logger,
         string projectName
