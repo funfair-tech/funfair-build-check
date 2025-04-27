9dc6a6e2c6efce35630b3e04301dc7cb1c6d67ce
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 06:41:11 +0000

    Cleanup: NoPreReleaseNuGetPackages.cs

--- src/FunFair.BuildCheck.ProjectChecks/Settings/NoPreReleaseNuGetPackages.cs
+++ src/FunFair.BuildCheck.ProjectChecks/Settings/NoPreReleaseNuGetPackages.cs
@@ -16,10 +16,7 @@ public sealed class NoPreReleaseNuGetPackages : IProjectCheck
     private readonly ICheckConfiguration _configuration;
     private readonly ILogger<NoPreReleaseNuGetPackages> _logger;
 
-    public NoPreReleaseNuGetPackages(
-        ICheckConfiguration configuration,
-        ILogger<NoPreReleaseNuGetPackages> logger
-    )
+    public NoPreReleaseNuGetPackages(ICheckConfiguration configuration, ILogger<NoPreReleaseNuGetPackages> logger)
     {
         this._configuration = configuration;
         this._logger = logger;
@@ -32,21 +29,13 @@ public sealed class NoPreReleaseNuGetPackages : IProjectCheck
             // check for private asset (if it's private the build won't fail for a pre-release package)
             string? privateAssets = package.GetAttributeOrElement("PrivateAssets");
 
-            this.CheckReference(
-                projectName: project.Name,
-                privateAssets: privateAssets,
-                package: package
-            );
+            this.CheckReference(projectName: project.Name, privateAssets: privateAssets, package: package);
         }
 
         return ValueTask.CompletedTask;
     }
 
-    private void CheckReference(
-        string projectName,
-        string? privateAssets,
-        in PackageReference package
-    )
+    private void CheckReference(string projectName, string? privateAssets, in PackageReference package)
     {
         if (
             !string.IsNullOrEmpty(privateAssets)
@@ -68,30 +57,18 @@ public sealed class NoPreReleaseNuGetPackages : IProjectCheck
             return;
         }
 
-        this._logger.FoundNuGetPackageAtVersion(
-            projectName: projectName,
-            packageId: package.Id,
-            version: version
-        );
+        this._logger.FoundNuGetPackageAtVersion(projectName: projectName, packageId: package.Id, version: version);
 
         if (!NuGetVersion.TryParse(value: version, out NuGetVersion? nuGetVersion))
         {
-            this._logger.CouldNotParseVersion(
-                projectName: projectName,
-                packageId: package.Id,
-                version: version
-            );
+            this._logger.CouldNotParseVersion(projectName: projectName, packageId: package.Id, version: version);
 
             return;
         }
 
         if (nuGetVersion.IsPrerelease && !this._configuration.PreReleaseBuild)
         {
-            this._logger.UsesPreReleaseVersion(
-                projectName: projectName,
-                packageId: package.Id,
-                version: version
-            );
+            this._logger.UsesPreReleaseVersion(projectName: projectName, packageId: package.Id, version: version);
         }
     }
 }
