f2ad61d39e79a2237c73c46f8a43c5b5387205cf
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 06:43:36 +0000

    Cleanup: NuGetPolicyDisableImplicitNuGetFallbackFolder.cs

--- src/FunFair.BuildCheck.ProjectChecks/Settings/NuGetPolicyDisableImplicitNuGetFallbackFolder.cs
+++ src/FunFair.BuildCheck.ProjectChecks/Settings/NuGetPolicyDisableImplicitNuGetFallbackFolder.cs
@@ -4,12 +4,6 @@ namespace FunFair.BuildCheck.ProjectChecks.Settings;
 
 public sealed class NuGetPolicyDisableImplicitNuGetFallbackFolder : SimplePropertyProjectCheckBase
 {
-    public NuGetPolicyDisableImplicitNuGetFallbackFolder(
-        ILogger<NuGetPolicyDisableImplicitNuGetFallbackFolder> logger
-    )
-        : base(
-            propertyName: "DisableImplicitNuGetFallbackFolder",
-            requiredValue: "true",
-            logger: logger
-        ) { }
+    public NuGetPolicyDisableImplicitNuGetFallbackFolder(ILogger<NuGetPolicyDisableImplicitNuGetFallbackFolder> logger)
+        : base(propertyName: "DisableImplicitNuGetFallbackFolder", requiredValue: "true", logger: logger) { }
 }
