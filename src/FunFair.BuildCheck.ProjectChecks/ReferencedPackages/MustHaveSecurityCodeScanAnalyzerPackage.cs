2ddf05ffee6f1268d62f92c1a359499d44701f07
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 05:15:28 +0000

    Cleanup: MustHaveSecurityCodeScanAnalyzerPackage.cs

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustHaveSecurityCodeScanAnalyzerPackage.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustHaveSecurityCodeScanAnalyzerPackage.cs
@@ -4,8 +4,6 @@ namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages;
 
 public sealed class MustHaveSecurityCodeScanAnalyzerPackage : MustHaveAnalyzerPackage
 {
-    public MustHaveSecurityCodeScanAnalyzerPackage(
-        ILogger<MustHaveSecurityCodeScanAnalyzerPackage> logger
-    )
+    public MustHaveSecurityCodeScanAnalyzerPackage(ILogger<MustHaveSecurityCodeScanAnalyzerPackage> logger)
         : base(packageId: "SecurityCodeScan.VS2019", mustHave: true, logger: logger) { }
 }
