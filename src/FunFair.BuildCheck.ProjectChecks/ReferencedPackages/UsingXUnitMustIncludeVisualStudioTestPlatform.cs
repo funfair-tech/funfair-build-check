da0fb39046464d7f67ee15df1808ec04e283ed2a
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 06:01:25 +0000

    Cleanup: UsingXUnitMustIncludeVisualStudioTestPlatform.cs

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/UsingXUnitMustIncludeVisualStudioTestPlatform.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/UsingXUnitMustIncludeVisualStudioTestPlatform.cs
@@ -2,15 +2,8 @@ using Microsoft.Extensions.Logging;
 
 namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages;
 
-public sealed class UsingXUnitMustIncludeVisualStudioTestPlatform
-    : HasAppropriateNonAnalysisPackages
+public sealed class UsingXUnitMustIncludeVisualStudioTestPlatform : HasAppropriateNonAnalysisPackages
 {
-    public UsingXUnitMustIncludeVisualStudioTestPlatform(
-        ILogger<UsingXUnitMustIncludeVisualStudioTestPlatform> logger
-    )
-        : base(
-            detectPackageId: "xunit",
-            mustIncludePackageId: "Microsoft.NET.Test.Sdk",
-            logger: logger
-        ) { }
+    public UsingXUnitMustIncludeVisualStudioTestPlatform(ILogger<UsingXUnitMustIncludeVisualStudioTestPlatform> logger)
+        : base(detectPackageId: "xunit", mustIncludePackageId: "Microsoft.NET.Test.Sdk", logger: logger) { }
 }
