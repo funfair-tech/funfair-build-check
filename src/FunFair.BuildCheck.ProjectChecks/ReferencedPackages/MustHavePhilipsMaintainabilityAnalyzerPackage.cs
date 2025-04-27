7ba2955003810ed01e5546f27dd3b2147cc114fe
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 05:09:05 +0000

    Cleanup: MustHavePhilipsMaintainabilityAnalyzerPackage.cs

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustHavePhilipsMaintainabilityAnalyzerPackage.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustHavePhilipsMaintainabilityAnalyzerPackage.cs
@@ -4,12 +4,6 @@ namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages;
 
 public sealed class MustHavePhilipsMaintainabilityAnalyzerPackage : MustHaveAnalyzerPackage
 {
-    public MustHavePhilipsMaintainabilityAnalyzerPackage(
-        ILogger<MustHavePhilipsMaintainabilityAnalyzerPackage> logger
-    )
-        : base(
-            packageId: "Philips.CodeAnalysis.MaintainabilityAnalyzers",
-            mustHave: true,
-            logger: logger
-        ) { }
+    public MustHavePhilipsMaintainabilityAnalyzerPackage(ILogger<MustHavePhilipsMaintainabilityAnalyzerPackage> logger)
+        : base(packageId: "Philips.CodeAnalysis.MaintainabilityAnalyzers", mustHave: true, logger: logger) { }
 }
