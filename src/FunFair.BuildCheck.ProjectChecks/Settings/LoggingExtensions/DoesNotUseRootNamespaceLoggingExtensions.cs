5ee47b92cf4f8b2ccf943fcf7e0f9ceb9d5818f4
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 07:52:52 +0000

    Cleanup: DoesNotUseRootNamespaceLoggingExtensions.cs

--- src/FunFair.BuildCheck.ProjectChecks/Settings/LoggingExtensions/DoesNotUseRootNamespaceLoggingExtensions.cs
+++ src/FunFair.BuildCheck.ProjectChecks/Settings/LoggingExtensions/DoesNotUseRootNamespaceLoggingExtensions.cs
@@ -9,8 +9,5 @@ internal static partial class DoesNotUseRootNamespaceLoggingExtensions
         Level = LogLevel.Error,
         Message = "{projectName}: Uses the RootNamespace setting to rename the namespace, when it should not"
     )]
-    public static partial void UsesRootNamespace(
-        this ILogger<DoesNotUseRootNamespace> logger,
-        string projectName
-    );
+    public static partial void UsesRootNamespace(this ILogger<DoesNotUseRootNamespace> logger, string projectName);
 }
