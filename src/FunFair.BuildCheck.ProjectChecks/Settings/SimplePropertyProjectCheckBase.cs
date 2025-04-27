24d1aaf83ac2b80a43d1eb7976f7e2ac491a31d2
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 06:57:12 +0000

    Cleanup: SimplePropertyProjectCheckBase.cs

--- src/FunFair.BuildCheck.ProjectChecks/Settings/SimplePropertyProjectCheckBase.cs
+++ src/FunFair.BuildCheck.ProjectChecks/Settings/SimplePropertyProjectCheckBase.cs
@@ -12,11 +12,7 @@ public abstract class SimplePropertyProjectCheckBase : IProjectCheck
     private readonly string _propertyName;
     private readonly string _requiredValue;
 
-    protected SimplePropertyProjectCheckBase(
-        string propertyName,
-        string requiredValue,
-        ILogger logger
-    )
+    protected SimplePropertyProjectCheckBase(string propertyName, string requiredValue, ILogger logger)
     {
         this._propertyName = propertyName;
         this._requiredValue = requiredValue;
