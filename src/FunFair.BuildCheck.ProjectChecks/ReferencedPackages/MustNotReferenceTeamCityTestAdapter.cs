6336741f4ff44d18adc0fa36e0f2a2285a4d2acb
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 05:36:36 +0000

    Cleanup: MustNotReferenceTeamCityTestAdapter.cs

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustNotReferenceTeamCityTestAdapter.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustNotReferenceTeamCityTestAdapter.cs
@@ -5,6 +5,5 @@ namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages;
 public sealed class MustNotReferenceTeamCityTestAdapter : MustNotReferencePackages
 {
     public MustNotReferenceTeamCityTestAdapter(ILogger<MustNotReferenceTeamCityTestAdapter> logger)
-        : base(["TeamCity.VSTest.TestAdapter"], reason: "No longer using teamcity", logger: logger)
-    { }
+        : base(["TeamCity.VSTest.TestAdapter"], reason: "No longer using teamcity", logger: logger) { }
 }
