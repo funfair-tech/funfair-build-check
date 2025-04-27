f3002620ccc3dcfb6745bddfe4d14350eb48f6f7
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 06:04:58 +0000

    Cleanup: UsingXUnitV3ExtensibilityMustIncludeAnalyzer.cs

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/UsingXUnitV3ExtensibilityMustIncludeAnalyzer.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/UsingXUnitV3ExtensibilityMustIncludeAnalyzer.cs
@@ -4,12 +4,7 @@ namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages;
 
 public sealed class UsingXUnitV3ExtensibilityMustIncludeAnalyzer : HasAppropriateAnalysisPackages
 {
-    public UsingXUnitV3ExtensibilityMustIncludeAnalyzer(
-        ILogger<UsingXUnitV3ExtensibilityMustIncludeAnalyzer> logger
-    )
-        : base(
-            detectPackageId: "xunit.v3.extensibility.core",
-            mustIncludePackageId: "xunit.analyzers",
-            logger: logger
-        ) { }
+    public UsingXUnitV3ExtensibilityMustIncludeAnalyzer(ILogger<UsingXUnitV3ExtensibilityMustIncludeAnalyzer> logger)
+        : base(detectPackageId: "xunit.v3.extensibility.core", mustIncludePackageId: "xunit.analyzers", logger: logger)
+    { }
 }
