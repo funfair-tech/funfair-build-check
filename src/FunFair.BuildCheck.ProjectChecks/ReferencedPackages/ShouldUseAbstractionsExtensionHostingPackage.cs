1e51b2792f31955cc9164532124297ea085c7bbb
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 05:50:27 +0000

    Cleanup: ShouldUseAbstractionsExtensionHostingPackage.cs

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/ShouldUseAbstractionsExtensionHostingPackage.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/ShouldUseAbstractionsExtensionHostingPackage.cs
@@ -4,9 +4,7 @@ namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages;
 
 public sealed class ShouldUseAbstractionsExtensionHostingPackage : ShouldUseAlternatePackage
 {
-    public ShouldUseAbstractionsExtensionHostingPackage(
-        ILogger<ShouldUseAbstractionsExtensionHostingPackage> logger
-    )
+    public ShouldUseAbstractionsExtensionHostingPackage(ILogger<ShouldUseAbstractionsExtensionHostingPackage> logger)
         : base(
             matchPackageId: "Microsoft.Extensions.Hosting",
             usePackageId: "Microsoft.Extensions.Hosting.Abstractions",
