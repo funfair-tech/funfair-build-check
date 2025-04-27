0a5b828b35f7714397f83cfa79a9a90482682a33
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 07:43:39 +0000

    Cleanup: DoesNotUseDotNetCliToolReferenceLoggingExtensions.cs

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/LoggingExtensions/DoesNotUseDotNetCliToolReferenceLoggingExtensions.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/LoggingExtensions/DoesNotUseDotNetCliToolReferenceLoggingExtensions.cs
@@ -4,13 +4,6 @@ namespace FunFair.BuildCheck.ProjectChecks.ReferencedPackages.LoggingExtensions;
 
 internal static partial class DoesNotUseDotNetCliToolReferenceLoggingExtensions
 {
-    [LoggerMessage(
-        EventId = 0,
-        Level = LogLevel.Error,
-        Message = "{projectName}: Contains DotNetCliToolReference."
-    )]
-    public static partial void ContainsDotNetCliToolReference(
-        this ILogger logger,
-        string projectName
-    );
+    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "{projectName}: Contains DotNetCliToolReference.")]
+    public static partial void ContainsDotNetCliToolReference(this ILogger logger, string projectName);
 }
