6ce4f28f4f257266605a6d7b783d974233d01417
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-05-08 17:24:46 +0000

    Cleanup: HasAppropriateAnalysisPackages.cs

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/HasAppropriateAnalysisPackages.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/HasAppropriateAnalysisPackages.cs
@@ -31,18 +31,32 @@ public abstract class HasAppropriateAnalysisPackages : IProjectCheck
 
         foreach (PackageReference package in project.ReferencedPackageElements(this._logger))
         {
-            this.CheckPackageReference(projectName: project.Name, package: package, foundSourcePackage: ref foundSourcePackage, foundAnalyzerPackage: ref foundAnalyzerPackage);
+            this.CheckPackageReference(
+                projectName: project.Name,
+                package: package,
+                foundSourcePackage: ref foundSourcePackage,
+                foundAnalyzerPackage: ref foundAnalyzerPackage
+            );
         }
 
         if (foundSourcePackage && !foundAnalyzerPackage)
         {
-            this._logger.DidNotFindMustIncludePackageForDetectedPackage(projectName: project.Name, detectPackageId: this._detectPackageId, mustIncludePackageId: this._mustIncludePackageId);
+            this._logger.DidNotFindMustIncludePackageForDetectedPackage(
+                projectName: project.Name,
+                detectPackageId: this._detectPackageId,
+                mustIncludePackageId: this._mustIncludePackageId
+            );
         }
 
         return ValueTask.CompletedTask;
     }
 
-    private void CheckPackageReference(string projectName, in PackageReference package, ref bool foundSourcePackage, ref bool foundAnalyzerPackage)
+    private void CheckPackageReference(
+        string projectName,
+        in PackageReference package,
+        ref bool foundSourcePackage,
+        ref bool foundAnalyzerPackage
+    )
     {
         if (StringComparer.OrdinalIgnoreCase.Equals(x: this._detectPackageId, y: package.Id))
         {
@@ -53,11 +67,17 @@ public abstract class HasAppropriateAnalysisPackages : IProjectCheck
         {
             foundAnalyzerPackage = true;
 
-            if (!package.Attributes.TryGetValue(key: "PrivateAssets", out string? assets) || string.IsNullOrWhiteSpace(assets) || !StringComparer.Ordinal.Equals(x: assets, y: PACKAGE_PRIVATE_ASSETS))
+            if (
+                !package.Attributes.TryGetValue(key: "PrivateAssets", out string? assets)
+                || string.IsNullOrWhiteSpace(assets)
+                || !StringComparer.Ordinal.Equals(x: assets, y: PACKAGE_PRIVATE_ASSETS)
+            )
             {
-                this._logger.DoesNotReferenceMustIncludePackageIdWithAPrivateAssetsAttribute(projectName: projectName,
-                                                                                             privateAssets: PACKAGE_PRIVATE_ASSETS,
-                                                                                             mustIncludePackageId: this._mustIncludePackageId);
+                this._logger.DoesNotReferenceMustIncludePackageIdWithAPrivateAssetsAttribute(
+                    projectName: projectName,
+                    privateAssets: PACKAGE_PRIVATE_ASSETS,
+                    mustIncludePackageId: this._mustIncludePackageId
+                );
             }
         }
     }
