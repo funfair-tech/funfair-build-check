275a5e6c8e9cfc8b4dc86d53985aecb88a60ea16
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-05-08 17:12:47 +0000

    Cleanup: GlobalJsonIsLatest.cs

--- src/FunFair.BuildCheck.SolutionChecks/GlobalJsonIsLatest.cs
+++ src/FunFair.BuildCheck.SolutionChecks/GlobalJsonIsLatest.cs
@@ -40,13 +40,20 @@ public sealed class GlobalJsonIsLatest : ISolutionCheck
 
         try
         {
-            GlobalJsonPacket? p = JsonSerializer.Deserialize(json: content, jsonTypeInfo: MustBeSerializable.Default.GlobalJsonPacket);
+            GlobalJsonPacket? p = JsonSerializer.Deserialize(
+                json: content,
+                jsonTypeInfo: MustBeSerializable.Default.GlobalJsonPacket
+            );
 
             if (!string.IsNullOrWhiteSpace(p?.Sdk?.RollForward))
             {
                 if (!StringComparer.OrdinalIgnoreCase.Equals(x: p.Sdk.Version, y: this._dotnetVersion))
                 {
-                    this._logger.UsingIncorrectDotNetSdkVersion(solutionFileName: solutionFileName, projectVersion: p.Sdk.Version, dotnetVersion: this._dotnetVersion);
+                    this._logger.UsingIncorrectDotNetSdkVersion(
+                        solutionFileName: solutionFileName,
+                        projectVersion: p.Sdk.Version,
+                        dotnetVersion: this._dotnetVersion
+                    );
                 }
             }
             else
@@ -56,7 +63,12 @@ public sealed class GlobalJsonIsLatest : ISolutionCheck
         }
         catch (Exception exception)
         {
-            this._logger.FailedToReadGlobalJson(solutionFileName: solutionFileName, file: file, message: exception.Message, exception: exception);
+            this._logger.FailedToReadGlobalJson(
+                solutionFileName: solutionFileName,
+                file: file,
+                message: exception.Message,
+                exception: exception
+            );
         }
     }
 }
