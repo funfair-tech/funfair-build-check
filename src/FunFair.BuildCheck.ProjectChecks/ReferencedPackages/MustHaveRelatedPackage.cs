3115a5fdfbd179eb4898893b6f6b669f7e3bc307
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-05-08 17:33:40 +0000

    Cleanup: MustHaveRelatedPackage.cs

--- src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustHaveRelatedPackage.cs
+++ src/FunFair.BuildCheck.ProjectChecks/ReferencedPackages/MustHaveRelatedPackage.cs
@@ -41,7 +41,11 @@ public abstract class MustHaveRelatedPackage : IProjectCheck
 
         if (foundSourcePackage && !foundRelatedPackage)
         {
-            this._logger.DidNotFindRelatedPackageForDetectedPackage(projectName: project.Name, detectPackageId: this._detectPackageId, mustIncludePackageId: this._mustIncludePackageId);
+            this._logger.DidNotFindRelatedPackageForDetectedPackage(
+                projectName: project.Name,
+                detectPackageId: this._detectPackageId,
+                mustIncludePackageId: this._mustIncludePackageId
+            );
         }
 
         return ValueTask.CompletedTask;
