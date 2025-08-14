614cf866c95f379907de2258ead9033129a6a70e
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-08-14 16:11:19 +0000

    Cleanup: MustNotUseOpenApiAnalyzers.cs

--- src/FunFair.BuildCheck.ProjectChecks/Settings/MustNotUseOpenApiAnalyzers.cs
+++ src/FunFair.BuildCheck.ProjectChecks/Settings/MustNotUseOpenApiAnalyzers.cs
@@ -5,5 +5,5 @@ namespace FunFair.BuildCheck.ProjectChecks.Settings;
 public sealed class MustNotUseOpenApiAnalyzers : MustNotDefinePropertyProjectCheckBase
 {
     public MustNotUseOpenApiAnalyzers(ILogger<MustNotUseOpenApiAnalyzers> logger)
-        : base(propertyName: "IncludeOpenAPIAnalyzers",  logger: logger) { }
+        : base(propertyName: "IncludeOpenAPIAnalyzers", logger: logger) { }
 }
