5d1b59f60ec21249c110b1eeec1355676a6dce63
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-08-16 16:01:03 +0000

    Cleanup: NoDuplicateProjectSettingsLoggingExtensions.cs

--- src/FunFair.BuildCheck.ProjectChecks/Settings/LoggingExtensions/NoDuplicateProjectSettingsLoggingExtensions.cs
+++ src/FunFair.BuildCheck.ProjectChecks/Settings/LoggingExtensions/NoDuplicateProjectSettingsLoggingExtensions.cs
@@ -5,17 +5,43 @@ namespace FunFair.BuildCheck.ProjectChecks.Settings.LoggingExtensions;
 
 internal static partial class NoDuplicateProjectSettingsLoggingExtensions
 {
-    [LoggerMessage(EventId = 1, Level = LogLevel.Error, Message = "{projectName}: Property {propertyName} appears multiple times")]
-    public static partial void PropertyAppearsMultipleTimes(this ILogger<NoDuplicateProjectSettings> logger, string projectName, string propertyName);
+    [LoggerMessage(
+        EventId = 1,
+        Level = LogLevel.Error,
+        Message = "{projectName}: Property {propertyName} appears multiple times"
+    )]
+    public static partial void PropertyAppearsMultipleTimes(
+        this ILogger<NoDuplicateProjectSettings> logger,
+        string projectName,
+        string propertyName
+    );
 
-    [LoggerMessage(EventId = 2, Level = LogLevel.Error, Message = "{projectName}: Property {propertyName} appears multiple times in differing cases: {givenNames}")]
-    private static partial void PropertyAppearsInMultipleCases(this ILogger<NoDuplicateProjectSettings> logger, string projectName, string propertyName, string givenNames);
+    [LoggerMessage(
+        EventId = 2,
+        Level = LogLevel.Error,
+        Message = "{projectName}: Property {propertyName} appears multiple times in differing cases: {givenNames}"
+    )]
+    private static partial void PropertyAppearsInMultipleCases(
+        this ILogger<NoDuplicateProjectSettings> logger,
+        string projectName,
+        string propertyName,
+        string givenNames
+    );
 
-    public static void PropertyAppearsInMultipleCases(this ILogger<NoDuplicateProjectSettings> logger, string projectName, string propertyName, HashSet<string> givenNames)
+    public static void PropertyAppearsInMultipleCases(
+        this ILogger<NoDuplicateProjectSettings> logger,
+        string projectName,
+        string propertyName,
+        HashSet<string> givenNames
+    )
     {
         if (logger.IsEnabled(LogLevel.Error))
         {
-            logger.PropertyAppearsInMultipleCases(projectName: projectName, propertyName: propertyName, string.Join(separator: ", ", values: givenNames));
+            logger.PropertyAppearsInMultipleCases(
+                projectName: projectName,
+                propertyName: propertyName,
+                string.Join(separator: ", ", values: givenNames)
+            );
         }
     }
 }
