676a0e9b70d7397431bbf0dad8a686d11eee8632
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-05-22 17:21:33 +0100

    Revert "feat: enforce EnableTrimAnalyzer=true when IsTrimmable=true"
    This reverts commit 983ddc36c746bca214c504964b055710b9a55c96.

--- src/FunFair.BuildCheck.ProjectChecks/Settings/ProjectsShouldHaveTrimAnalyzerConfiguredPolicy.cs
+++ src/FunFair.BuildCheck.ProjectChecks/Settings/ProjectsShouldHaveTrimAnalyzerConfiguredPolicy.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using FunFair.BuildCheck.Interfaces;
@@ -11,7 +10,6 @@ namespace FunFair.BuildCheck.ProjectChecks.Settings;
 public sealed class ProjectsShouldHaveTrimAnalyzerConfiguredPolicy : IProjectCheck
 {
     private const string SETTING = "EnableTrimAnalyzer";
-    private const string IS_TRIMMABLE = "IsTrimmable";
 
     private readonly ILogger<ProjectsShouldHaveTrimAnalyzerConfiguredPolicy> _logger;
 
@@ -24,21 +22,9 @@ public sealed class ProjectsShouldHaveTrimAnalyzerConfiguredPolicy : IProjectChe
 
     public ValueTask CheckAsync(ProjectContext project, CancellationToken cancellationToken)
     {
-        string? trimAnalyzer = project.GetProperty(SETTING);
-
-        if (string.IsNullOrWhiteSpace(trimAnalyzer))
+        if (!project.HasProperty(SETTING))
         {
             this._logger.ProjectShouldConfigureTrimAnalyzer(projectName: project.Name, property: SETTING);
-
-            return ValueTask.CompletedTask;
-        }
-
-        string? isTrimmable = project.GetProperty(IS_TRIMMABLE);
-        bool trimmable = StringComparer.OrdinalIgnoreCase.Equals(x: isTrimmable, y: "true");
-
-        if (trimmable && !StringComparer.OrdinalIgnoreCase.Equals(x: trimAnalyzer, y: "true"))
-        {
-            this._logger.ProjectMustEnableTrimAnalyzerWhenTrimmable(projectName: project.Name, property: SETTING);
         }
 
         return ValueTask.CompletedTask;
